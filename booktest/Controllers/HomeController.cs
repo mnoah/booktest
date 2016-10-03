85e012ac28b1de9b179f01028598638484f432e2
mnoah <noahvrazo@gmail.com>
2016-10-02 20:45:41 -0400

    working

---
+++ booktest/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using booktest.Models;
+
+namespace booktest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good morning" : "good after noon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to party organizer
+                return View("thanks", guestResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ booktest/Models/GuestResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace booktest.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter your email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify your gender")]
+        public string Gender { get; set; }
+
+        //[Required(ErrorMessage = "Please specify whether you'll attend")]
+        //public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool Pet { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you live in Detorit")]
+        public bool? Live { get; set; }
+    }
+}
